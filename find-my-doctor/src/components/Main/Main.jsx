32f569069642e1e7b32f6b016e6df592768517ec
Emma Clark <emmabossuytclark@gmail.com>
2025-11-23 19:57:54 -0400

    Added code to populate the main component with Cards for each doctor

--- find-my-doctor/src/components/Main/Main.jsx
+++ find-my-doctor/src/components/Main/Main.jsx
@@ -1,9 +1,31 @@
-function Main() {
+import Card from "../Card/Card";
+import doctors from "../../Data/doctors.json";
+
+function Main({ selectedProvince }) {
+  const selectedDoctors =
+    doctors.length > 0
+      ? doctors.filter((element) => element.province === selectedProvince)
+      : [];
+
+  function createCards({ selectedDoctors }) {
+    return selectedDoctors.map((item) => {
+      return <Card key={item.name} doctor={item} />; // Using doctor's name an key
+    });
+  }
+
   return (
     <main
       className="d-flex flex-wrap p-3"
-      style={{ backgroundColor: "#d0ebff", gap: "1rem", flexGrow: 1 }}
-    ></main>
+      style={{
+        backgroundColor: "#d0ebff",
+        gap: "15px",
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "flex-start",
+      }}
+    >
+      {createCards({ selectedDoctors })}
+    </main>
   );
 }
 
