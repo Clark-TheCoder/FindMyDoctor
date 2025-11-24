1356ebb909797d24a4f2a90d4daf476bd75fb3d5
Emma Clark <emmabossuytclark@gmail.com>
2025-11-23 20:50:19 -0400

    Added router and routes to App.jsx

--- find-my-doctor/src/App.jsx
+++ find-my-doctor/src/App.jsx
@@ -1,16 +1,41 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import Home from "./pages/Home.jsx";
+// import { Router } from "react-router-dom";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       <Home></Home>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./pages/Home.jsx";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile.jsx";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Home></Home>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/doctor/:id" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
