7f4d6b2a6b22c0e5e6ed9f25a89d7d52e73cb111
Emma Clark <emmabossuytclark@gmail.com>
2025-11-24 11:21:08 -0400

    Added Header, DoctorDetails, and HomeButton component to Profile page

--- find-my-doctor/src/pages/Profile.jsx
+++ find-my-doctor/src/pages/Profile.jsx
@@ -1,5 +1,25 @@
+import { useParams } from "react-router-dom";
+import { Fragment } from "react";
+import doctors from "../Data/doctors.json";
+import Header from "../components/Header/Header.jsx";
+import DoctorDetails from "../components/DoctorDetails/DoctorDetails.jsx";
+import HomeButton from "../components/HomeBtn/HomeBtn";
+
 function Profile() {
-  return <div>Hello World</div>;
+  const params = useParams();
+  const doctorId = params.id;
+
+  const doctor = doctors.find((item) => {
+    return String(item.id) === doctorId;
+  });
+
+  return (
+    <>
+      <Header title={doctor.name}></Header>
+      <DoctorDetails doctor={doctor}></DoctorDetails>
+      <HomeButton homeRoute={"/"}></HomeButton>
+    </>
+  );
 }
 
 export default Profile;
